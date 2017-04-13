ed3a591e14dd400f150d38053b0f778aee401232
Nick Liu <nickliu0211@gmail.com>
2017-04-13 12:39:46 -0500

    added files

---
+++ app/project-list/project-list.component.js
@@ -0,0 +1,7 @@
+angular.
+module('projectList').
+component('projectList', {
+  // Note: The URL is relative to our `index.html` file
+  templateUrl: 'project-list/project-list.template.html',
+  controller: ...
+});
